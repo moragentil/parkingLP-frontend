955280e1473b223484b6fc438fe3c6cc76724902
moragentil <moragentil@gmail.com>
2025-07-07 11:17:22 -0300

    estilos agregados y dependencias de iconos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import tw from '../utils/tailwind';
+import sharedStyles from '../utils/sharedStyles'; 
 
 export default function Header({ title }) {
   return (
-    <View style={tw`flex-row items-center justify-between bg-blue-500 pt-20 pb-6 px-4`}>
+    <View style={[tw`flex-row items-center justify-between bg-blue-500 pt-20 pb-6 px-4`, sharedStyles.bgCustomBlue]}>
       <TouchableOpacity>
-        <Text style={tw`text-white text-2xl`}>☰</Text> {/* Menú sandwich */}
+        <Text style={tw`text-white text-2xl`}>☰</Text> 
       </TouchableOpacity>
       <Text style={tw`text-white text-xl font-bold`}>{title}</Text>
-      <View style={tw`w-8`} /> {/* Espaciador para mantener el diseño */}
+      <View style={tw`w-8`} /> 
     </View>
   );
 }
--- src/utils/sharedStyles.js
+++ src/utils/sharedStyles.js
@@ -4,6 +4,12 @@ const sharedStyles = StyleSheet.create({
   bgCustomBlue: {
     backgroundColor: '#3236FF',
   },
+  borderColor: {
+    borderColor: '#3236FF',
+  },
+  textColor: {
+    color: '#2B2B2B',
+  },
 });
 
 export default sharedStyles;
