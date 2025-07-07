2700baa4ed20f1546731dc4155104c9cc9c66f06
moragentil <moragentil@gmail.com>
2025-07-07 20:58:44 -0300

    estilos ajustados de login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -18,34 +18,35 @@ export default function LoginScreen({ navigation }) {
   return (
     <View style={[tw`flex-1 items-center justify-center`, sharedStyles.bgCustomBlue]}>
       <Text style={tw`text-3xl font-bold text-white mb-2`}>Parking LP</Text>
-      <Text style={tw`text-lg text-white mb-8`}>Estacionamiento Inteligente</Text>
-      <View style={tw`bg-white rounded-lg p-6 w-80`}>
-        <Text style={tw`text-xl font-bold mb-4`}>Iniciar Sesión</Text>
-        <Text style={tw`text-gray-600 mb-4`}>Accede a tu cuenta</Text>
+      <Text style={tw`text-base text-white mb-8`}>Estacionamiento Inteligente</Text>
+      <View style={tw`bg-white  rounded-lg p-6 w-80`}>
+        <Text style={tw`text-2xl text-gray-800 text-center font-bold mb-2`}>Iniciar Sesión</Text>
+        <Text style={tw`text-gray-600 text-center mb-4`}>Accede a tu cuenta</Text>
         <TextInput
-          style={tw`border border-gray-300 rounded px-4 py-2 mb-4`}
+          style={tw`border border-gray-300 rounded-lg p-3 mb-4`}
           placeholder="Teléfono o Email"
           value={username}
           onChangeText={setUsername}
         />
         <TextInput
-          style={tw`border border-gray-300 rounded px-4 py-2 mb-4`}
+          style={tw`border border-gray-300 rounded-lg p-3 mb-4`}
           placeholder="Contraseña"
           secureTextEntry
           value={password}
           onChangeText={setPassword}
         />
         <TouchableOpacity
-          style={tw`bg-blue-500 rounded py-2`}
+          style={[tw` rounded py-3`, sharedStyles.bgCustomBlue]}
           onPress={handleLogin}
         >
           <Text style={tw`text-white text-center font-bold`}>Entrar</Text>
         </TouchableOpacity>
+        <Text style={[tw`text-gray-600 mt-4 text-center`]}>¿No tienes cuenta? </Text>
         <TouchableOpacity
           onPress={() => navigation.navigate('Register')}
           style={tw`mt-4`}
         >
-          <Text style={tw`text-blue-500 text-center`}>¿No tienes cuenta? Registrarse</Text>
+          <Text style={[tw` text-center`, sharedStyles.textColorBlue]}>Registrate</Text>
         </TouchableOpacity>
       </View>
     </View>
